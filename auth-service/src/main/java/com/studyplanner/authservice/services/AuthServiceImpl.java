c2df020a114a6692d4a802528703a267b90a6254
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-01 21:39:19 +0100

    implement login API

--- auth-service/src/main/java/com/studyplanner/authservice/services/AuthServiceImpl.java
+++ auth-service/src/main/java/com/studyplanner/authservice/services/AuthServiceImpl.java
@@ -1,41 +1,48 @@
 package com.studyplanner.authservice.services;
 
 import com.studyplanner.authservice.dto.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class AuthServiceImpl implements AuthService{
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
     @Override
     public ResponseEntity<?> login(LoginRequest loginRequest) {
 
-        //authenticationManager checks credentials i.e authenticate user
+        try {
+            //authenticationManager checks credentials i.e authenticate user
 
-        // DaoAuthenticationProvider - > UserDetailsService -> load user from DB
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername()
+                    , loginRequest.getPassword()));
 
-        // if password matches
+            // DaoAuthenticationProvider - > UserDetailsService -> load user from DB
+            // if password matches
 
-        // JwtUtil generates token
+            // JwtUtil generates token
+            String token= jwtUtil.generateToken(loginRequest.getUsername());
 
-        return null;
-    }
-}
+            // return Token
+            return ResponseEntity.ok(Map.of("AuthToken", token));
 
-public ResponseEntity<?> login(LoginRequest loginRequest) {
-    try {
-        // Authenticate user
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
-        );
-
-        // Generate token
-        String token = jwtUtil.generateToken(loginRequest.getUsername());
-
-        return ResponseEntity.ok(new AuthResponse(token));
-    } catch (BadCredentialsException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of("message", "Invalid username or password"));
+            return null;
+        }catch (BadCredentialsException e){
+           return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message","Invalid username or password"));
+        }
     }
 }
 
