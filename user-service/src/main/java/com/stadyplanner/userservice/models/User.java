93ca463134b225c1479cfdd72d0d750059ae81c9
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-01 18:00:16 +0100

    add user entity

--- user-service/src/main/java/com/stadyplanner/userservice/models/User.java
+++ user-service/src/main/java/com/stadyplanner/userservice/models/User.java
@@ -1,6 +1,15 @@
 package com.stadyplanner.userservice.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -8,10 +17,31 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
+@Entity
 public class User {
 
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private long userId;
+
+    @Column(name="username")
     private String username;
+
+    @Column(name="password")
     private String password;
+
+    @Column(name="mail_id")
+    private String mailId;
+
+    @Column(name="role")
     private String role;
+
+    @CreationTimestamp
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
 }
