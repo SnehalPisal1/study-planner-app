bbd7a444c4786b2202517995a65c383235d3f144
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-04 18:32:04 +0100

    Add Eureka Client support to task-service

--- task-service/src/main/java/com/studyplanner/taskservice/TaskServiceApplication.java
+++ task-service/src/main/java/com/studyplanner/taskservice/TaskServiceApplication.java
@@ -24,11 +24,13 @@ package com.studyplanner.taskservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.studyplanner.taskservice.repositories")
 @EntityScan("com.studyplanner.taskservice.models") // Scan entities
+@EnableDiscoveryClient
 public class TaskServiceApplication {
 
     public static void main(String[] args){
