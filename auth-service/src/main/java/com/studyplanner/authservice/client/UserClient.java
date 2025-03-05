746250db0177439ca749d331a4700fd6d627cc4d
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-05 18:19:04 +0100

    add Feign client interface for user-service communication

---
+++ auth-service/src/main/java/com/studyplanner/authservice/client/UserClient.java
@@ -0,0 +1,15 @@
+package com.studyplanner.authservice.client;
+
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@FeignClient(name = "user-service")
+public interface UserClient {
+    @GetMapping("/users/{username}")
+    ResponseEntity<?> getUserByUsername(@Valid @PathVariable String username);
+}
