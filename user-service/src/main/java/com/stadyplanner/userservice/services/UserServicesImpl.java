a914f77598a50e081bb8f4a98f106ba8b06e07d4
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-05 18:19:50 +0100

    Add logging to user-service for better debugging

--- user-service/src/main/java/com/stadyplanner/userservice/services/UserServicesImpl.java
+++ user-service/src/main/java/com/stadyplanner/userservice/services/UserServicesImpl.java
@@ -2,14 +2,17 @@ package com.stadyplanner.userservice.services;
 
 import com.stadyplanner.userservice.models.User;
 import com.stadyplanner.userservice.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-
 @Service
 public class UserServicesImpl implements UserServices{
+
+    private final static Logger LOGGER= LoggerFactory.getLogger(UserServicesImpl.class);
 
     @Autowired
     UserRepository userRepository;
@@ -21,6 +24,7 @@ public class UserServicesImpl implements UserServices{
 
     @Override
     public Optional<User> getUserByUsername(String username) {
+        LOGGER.info("Get User By Username : "+username);
         return userRepository.findByUsername(username);
     }
 }
