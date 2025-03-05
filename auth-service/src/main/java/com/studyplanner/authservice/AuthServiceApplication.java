3cc239ce94bc0bba077c9d67f5068b9464118335
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-05 18:22:32 +0100

    Enable Feign Clients

--- auth-service/src/main/java/com/studyplanner/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/studyplanner/authservice/AuthServiceApplication.java
@@ -3,14 +3,14 @@ package com.studyplanner.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class AuthServiceApplication {
 
     public static void main(String[] args){
-
-        System.out.println("Executing AuthServiceApplication.....");
 
         SpringApplication.run(AuthServiceApplication.class,args);
     }
