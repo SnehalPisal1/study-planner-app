1e0dd1cbe6695a3ae99b95f64aad375fd8c9a5af
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-05 18:22:58 +0100

    Integrate user-service API

--- auth-service/src/main/java/com/studyplanner/authservice/services/AuthServiceImpl.java
+++ auth-service/src/main/java/com/studyplanner/authservice/services/AuthServiceImpl.java
@@ -1,10 +1,12 @@
 package com.studyplanner.authservice.services;
 
+import com.studyplanner.authservice.AuthServiceApplication;
 import com.studyplanner.authservice.dto.LoginRequest;
 import com.studyplanner.authservice.securityUtility.JwtUtil;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.slf4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -16,6 +18,8 @@ import java.util.Map;
 @Service
 public class AuthServiceImpl implements AuthService{
 
+    private final static Logger LOGGER= LoggerFactory.getLogger(AuthServiceImpl.class);
+
     @Autowired
     AuthenticationManager authenticationManager;
 
@@ -26,22 +30,18 @@ public class AuthServiceImpl implements AuthService{
     public ResponseEntity<?> login(LoginRequest loginRequest) {
 
         try {
-            //authenticationManager checks credentials i.e authenticate user
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername()
+            LOGGER.info("authenticationManager checks credentials");
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getUsername(),loginRequest.getPassword()));
 
-                    , loginRequest.getPassword()));
-
-            // TODO - DaoAuthenticationProvider - > UserDetailsService -> load user from DB
-            // if password matches
-
-            // JwtUtil generates token
+            LOGGER.info("JwtUtil generates token");
             String token= jwtUtil.generateToken(loginRequest.getUsername());
 
-            // return Token
             return ResponseEntity.ok(Map.of("AuthToken", token));
 
         }catch (BadCredentialsException e){
-           return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message","Invalid username or password"));
+           return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message","Invalid username or password" + e));
         }
     }
 }
--- auth-service/src/main/java/com/studyplanner/authservice/services/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/studyplanner/authservice/services/CustomUserDetailsService.java
@@ -1,5 +1,11 @@
 package com.studyplanner.authservice.services;
 
+import com.studyplanner.authservice.client.UserClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,22 +13,39 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final static Logger LOGGER= LoggerFactory.getLogger(CustomUserDetailsService.class);
+
+    @Autowired
+    UserClient userClient; // Feign Client for User Service
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-       /* // Fetch user from the database
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                new ArrayList<>() // Add authorities/roles here
-        ); */
+        try {
+            LOGGER.info("Call User API to fetch user by username");
+            ResponseEntity<?> responseEntity = userClient.getUserByUsername(username);
 
-        return new User("snehal-pisal5", "12345678", new ArrayList<>());
+            LOGGER.info("Check if user exists - status code : {}", responseEntity.getStatusCode());
+
+            if ((responseEntity.getStatusCode() == HttpStatus.OK && responseEntity.getBody() != null)) {
+                LOGGER.info("OK : {}", responseEntity.getBody());
+                LinkedHashMap<String, String> userInfo = (LinkedHashMap<String, String>) responseEntity.getBody();
+                LOGGER.info("userInfo: {}", userInfo);
+
+                return new User(userInfo.get("username"), userInfo.get("password"), new ArrayList<>());
+
+            } else {
+                throw new UsernameNotFoundException("User not found with username: " + username);
+            }
+        } catch (Exception e) {
+            // Handle any unexpected errors during user retrieval
+            throw new UsernameNotFoundException("Error authenticating user: " + username, e);
+        }
     }
 }
-
