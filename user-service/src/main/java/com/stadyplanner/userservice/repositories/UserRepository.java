579704d18f4e0c0fdfa8acce7388bfc55276724b
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-08 22:47:21 +0100

    Correct create User API

--- user-service/src/main/java/com/stadyplanner/userservice/repositories/UserRepository.java
+++ user-service/src/main/java/com/stadyplanner/userservice/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.stadyplanner.userservice.repositories;
 
 import com.stadyplanner.userservice.models.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
 
+    boolean existsByUsername(@NotNull(message = "User name should not be null") String username);
+
+    boolean existsByMailId(@NotNull @Email(message = "Invalid email format") String mailId);
 }
--- user-service/src/main/java/com/stadyplanner/userservice/services/UserServicesImpl.java
+++ user-service/src/main/java/com/stadyplanner/userservice/services/UserServicesImpl.java
@@ -1,10 +1,12 @@
 package com.stadyplanner.userservice.services;
 
+import com.stadyplanner.userservice.models.Role;
 import com.stadyplanner.userservice.models.User;
 import com.stadyplanner.userservice.repositories.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,11 +16,33 @@ public class UserServicesImpl implements UserServices{
 
     private final static Logger LOGGER= LoggerFactory.getLogger(UserServicesImpl.class);
 
+    PasswordEncoder passwordEncoder;
+
     @Autowired
     UserRepository userRepository;
 
     @Override
     public User createUser(User user) {
+
+        // Validate uniqueness of username and email
+        if (userRepository.existsByUsername(user.getUsername())) {
+            LOGGER.info("Username already exists");
+            throw new IllegalArgumentException("Username already exists");
+        }
+        if (userRepository.existsByMailId(user.getMailId())) {
+            LOGGER.info("Email already exists");
+            throw new IllegalArgumentException("Email already exists");
+        }
+
+        // Hash the password before saving
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        /* Ensure default role is set (if not provided)
+        if (user.getRole() == null) {
+            user.setRole(Role.USER);
+        } */
+
         return userRepository.save(user);
     }
 
