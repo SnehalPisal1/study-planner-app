a5de3858bbf7b15a031e8eba6e8ceafad9184e2f
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-10 20:33:41 +0100

    fix : receive a password field in the user data

--- user-service/src/main/java/com/stadyplanner/userservice/models/User.java
+++ user-service/src/main/java/com/stadyplanner/userservice/models/User.java
@@ -1,7 +1,5 @@
 package com.stadyplanner.userservice.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
@@ -23,43 +21,31 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="user_id")
+    @Column(name = "user_id")
     private long userId;
 
     @NotNull(message = "User name should not be null")
-    @Column(name="username" , unique = true , nullable = false)
+    @Column(name = "username", unique = true, nullable = false)
     private String username;
 
     @NotNull
-    @Column(name="password", nullable = false)
+    @Column(name = "password", nullable = false)
     private String password;
 
     @NotNull
     @Email(message = "Invalid email format")
-    @Column(name="mail_id", unique = true, nullable = false)
+    @Column(name = "mail_id", unique = true, nullable = false)
     private String mailId;
 
     @Enumerated(EnumType.STRING)
-    @Column(name="role")
+    @Column(name = "role")
     private Role role = Role.USER;
 
     @CreationTimestamp
-    @Column(name="created_at")
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
-    @Column(name="updated_at")
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-
-    // Excludes password from ALL serialization (responses)
-    // Explicitly define the getter and annotate it with @JsonIgnore
-    @JsonIgnore
-    public String getPassword() {
-        return password;
-    }
-
-    @JsonProperty // Allows password to be set from the request
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
