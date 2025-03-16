cb62c79700173fab7e77d961523bbcac750c29fb
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-16 17:12:20 +0100

    hide some fields from request body

--- task-service/src/main/java/com/studyplanner/taskservice/models/Task.java
+++ task-service/src/main/java/com/studyplanner/taskservice/models/Task.java
@@ -1,6 +1,9 @@
 package com.studyplanner.taskservice.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -20,28 +23,36 @@ public class Task {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name="task_id")
+    @Schema(hidden = true)
     private long taskId;
 
+    @NotBlank(message = "Task name is required")
     @Column(name="task_name")
     private String taskName;
 
     @Column(name="description")
     private String description;
 
+    @NotBlank(message = "Status is required")
     @Column(name="status")
     private String status; //"To Do", "In Progress", "Done"
 
+    @FutureOrPresent(message = "Due date must be in the future")
     @Column(name="due_date")
     private LocalDateTime dueDate;
 
     @CreationTimestamp
+    @Schema(hidden = true)
     @Column(name="created_at")
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
+    @Schema(hidden = true)
     @Column(name="updated_at")
     private LocalDateTime updatedAt;
 
     @Column(name="created_by")
+    @Schema(hidden = true)
     private String createdBy;
 }
+
