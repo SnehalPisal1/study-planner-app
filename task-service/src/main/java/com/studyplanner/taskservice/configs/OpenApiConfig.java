6eec0268e7df67092b340cb68cdb323fcd60f91f
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-16 16:58:20 +0100

    configure openApi for Task APIs

---
+++ task-service/src/main/java/com/studyplanner/taskservice/configs/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.studyplanner.taskservice.configs;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task Service API")
+                        .description("1.0.0")
+                        .version("APIs for managing study plans and tasks"));
+
+    }
+
+}
