979ff3d2bcf9de9cf3b4b5bbec35c98b0aff600f
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-18 20:42:00 +0100

    fix: fetch correct token

--- task-service/src/main/java/com/studyplanner/taskservice/configs/JwtUtil.java
+++ task-service/src/main/java/com/studyplanner/taskservice/configs/JwtUtil.java
@@ -1,40 +1,24 @@
 package com.studyplanner.taskservice.configs;
 
 
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.HashMap;
 
 @Component
 public class JwtUtil {
 
     // secret key - from properties
     @Value("${jwt.secret}")
-    private String SECRET_KEY;
-
-    // expiration -from properties
-    @Value("${jwt.expiration}")
-    private long expiration;
-
-    //Generate token
-    public String generateToken(String username){
-            return Jwts.builder()
-                    .setClaims( new HashMap<>())
-                    .setSubject(username)
-                    .setIssuedAt(new Date(System.currentTimeMillis()))
-                    .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
-                    .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                    .compact();
-        }
-
+    String SECRET_KEY;
 
     public boolean validateToken(String token) {
         try {
+            if (token != null && token.startsWith("Bearer ")) {
+                token = token.substring(7);
+            }
             Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token);
             return !isTokenExpired(token);
         } catch (JwtException | IllegalArgumentException e) {
@@ -54,15 +38,6 @@ public class JwtUtil {
     private Date extractExpiration(String token) {
         return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getExpiration();
     }
-
-
-  /*  public Boolean validateToken(String token, String userDetails) {
-            final String username = extractUsername(token);
-            return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-        } */
-
-
-
 
 }
 
