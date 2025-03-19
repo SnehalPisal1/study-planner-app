caa01396469e5d4f1c52ee0d90ff832780dce23e
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-19 23:43:06 +0100

    Refactor code

--- task-service/src/main/java/com/studyplanner/taskservice/controllers/TaskController.java
+++ task-service/src/main/java/com/studyplanner/taskservice/controllers/TaskController.java
@@ -18,7 +18,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@RequestMapping("/tasks")
 @Tag(name = "Task Management API", description = "API for managing tasks")
+@SecurityRequirement(name = "bearerAuth")
 public class TaskController {
 
     public static Logger LOGGER = LoggerFactory.getLogger(TaskController.class);
@@ -28,12 +30,10 @@ public class TaskController {
     @Autowired
     JwtUtil jwtUtil;
 
-    @PostMapping("/tasks")
     @Operation(
             summary = "Create a new task",
             description = "Creates a task with due date"
     )
-    @SecurityRequirement(name = "bearerAuth")
     @ApiResponse(responseCode = "201", description = "Task created successfully")
     @ApiResponse(responseCode = "401", description = "Unauthorized")
     @ApiResponse(responseCode = "500", description = "Internal Server Error")
@@ -61,11 +61,9 @@ public class TaskController {
             summary = "Get a all task",
             description = "Get a all task for specified user"
     )
-    @SecurityRequirement(name = "bearerAuth")
     @ApiResponse(responseCode = "200", description = "Retrieve a list of all tasks")
     @ApiResponse(responseCode = "401", description = "Unauthorized")
     @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    @GetMapping("/tasks")
     public ResponseEntity<?> getAllTasks(@RequestHeader("Authorization") String token) {
 
         try {
@@ -95,11 +93,10 @@ public class TaskController {
             summary = "Delete task ",
             description = "Delete task from task List"
     )
-    @SecurityRequirement(name = "bearerAuth")
     @ApiResponse(responseCode = "200", description = "Task deleted successfully")
     @ApiResponse(responseCode = "401", description = "Unauthorized")
     @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    @DeleteMapping("/tasks/{taskId}")
+    @DeleteMapping("/{taskId}")
     public ResponseEntity<?> deleteTask(@PathVariable long taskId, @RequestHeader("Authorization") String token) {
 
         try {
@@ -129,11 +126,10 @@ public class TaskController {
             summary = "Update an existing task",
             description = "Modify task details such as title, description, due date, or status using the task's unique ID. Returns the updated task."
     )
-    @SecurityRequirement(name = "bearerAuth")
     @ApiResponse(responseCode = "200", description = "Task updated successfully")
     @ApiResponse(responseCode = "401", description = "Unauthorized")
     @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    @PutMapping("/tasks/{taskId}")
+    @PutMapping("/{taskId}")
     public ResponseEntity<?> updateTask(@PathVariable Long taskId, @Valid @RequestBody Task task,
                                         @RequestHeader("Authorization") String token) {
         try {
