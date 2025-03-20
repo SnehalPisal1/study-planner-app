cb5095181b715ccde363a2513be5c826b27e723e
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-20 20:41:40 +0100

    modify security configs

--- task-service/src/main/java/com/studyplanner/taskservice/configs/SecurityConfigs.java
+++ task-service/src/main/java/com/studyplanner/taskservice/configs/SecurityConfigs.java
@@ -1,6 +1,6 @@
 package com.studyplanner.taskservice.configs;
 
-import jakarta.ws.rs.HttpMethod;
+import com.studyplanner.taskservice.SecurityUtility.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +8,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,23 +21,28 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfigs {
 
     @Autowired
-    private JwtAuthFilter jwtAuthFilter;
+    JwtAuthFilter jwtAuthFilter;
+
+    @Autowired
+    AuthenticationEntryPoint jwtAuthEntryPoint;
 
     //use filter chain to intercept request, enforce security rules and manages sessions or tokens
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        return http.
+                 csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/tasks/**").authenticated() // Require auth for /tasks
-                        .anyRequest().permitAll() // Allow all other requests without auth
+                        .anyRequest().authenticated()
                 )
+                .exceptionHandling(exc
+                        -> exc.authenticationEntryPoint(jwtAuthEntryPoint))
                 .sessionManagement(session
                         -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
 
-        ; // JWT used for Stateless authentication
-        return http.build();
     }
 
     @Bean
