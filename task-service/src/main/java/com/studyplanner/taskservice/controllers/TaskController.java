472c0ece800688fcf6dc1f20ea3e5390133fc289
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-22 11:58:23 +0100

    remove @RequestHeader from Get API

--- task-service/src/main/java/com/studyplanner/taskservice/controllers/TaskController.java
+++ task-service/src/main/java/com/studyplanner/taskservice/controllers/TaskController.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -67,16 +69,11 @@ public class TaskController {
     @ApiResponse(responseCode = "200", description = "Retrieve a list of all tasks")
     @ApiResponse(responseCode = "401", description = "Unauthorized")
     @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    public ResponseEntity<?> getAllTasks(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<?> getAllTasks() {
 
         try {
-            // Check authentication first
-            if (token == null || token.isEmpty() || !jwtUtil.validateToken(token)) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                        .body(Map.of("message", "Authentication required"));
-            }
-
-            String userName = extractUserNameFromToken(token);
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            String userName =  auth.getName();;
             List<Task> response = taskServicesImpl.getAllTasks(userName);
             if (response != null && !response.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.OK).body(response);
