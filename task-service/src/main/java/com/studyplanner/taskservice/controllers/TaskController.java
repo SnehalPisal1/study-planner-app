1349e09eb86c82eda8460531a213189a27be3713
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-03-22 11:44:21 +0100

    remove unused imports

--- task-service/src/main/java/com/studyplanner/taskservice/controllers/TaskController.java
+++ task-service/src/main/java/com/studyplanner/taskservice/controllers/TaskController.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
