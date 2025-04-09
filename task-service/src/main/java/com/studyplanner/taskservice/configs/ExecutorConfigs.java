cc60ad20f29a27405f45a8f6f91c2b505fd1c267
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-04-09 21:19:41 +0200

    remove unused imports

--- task-service/src/main/java/com/studyplanner/taskservice/configs/ExecutorConfigs.java
+++ task-service/src/main/java/com/studyplanner/taskservice/configs/ExecutorConfigs.java
@@ -1,9 +1,7 @@
 package com.studyplanner.taskservice.configs;
 
-import com.studyplanner.taskservice.services.TaskServicesImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.config.MethodInvokingFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;
