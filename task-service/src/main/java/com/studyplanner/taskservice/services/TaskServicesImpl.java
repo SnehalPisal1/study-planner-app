66e42f04827e3aa8e3f310d63676cd5b4bdc2d3b
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-05-01 19:02:42 +0200

    refactor code

--- task-service/src/main/java/com/studyplanner/taskservice/services/TaskServicesImpl.java
+++ task-service/src/main/java/com/studyplanner/taskservice/services/TaskServicesImpl.java
@@ -2,6 +2,8 @@ package com.studyplanner.taskservice.services;
 
 import com.studyplanner.taskservice.models.Task;
 import com.studyplanner.taskservice.repositories.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
