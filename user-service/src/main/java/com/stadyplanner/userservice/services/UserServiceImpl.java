4d518815c2f99f95b8064aa30f6f9b6056d197ec
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-05-01 18:31:28 +0200

    refactor code

--- user-service/src/main/java/com/stadyplanner/userservice/services/UserServiceImpl.java
+++ user-service/src/main/java/com/stadyplanner/userservice/services/UserServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class UserServicesImpl implements UserServices{
+public class UserServiceImpl implements UserService {
 
     private final static Logger LOGGER= LoggerFactory.getLogger(UserServicesImpl.class);
 
