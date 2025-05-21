ccb52f534da8b38c14e7647a3a7021b87d070810
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-05-21 11:36:25 +0200

    refactor

--- task-service/src/main/java/com/studyplanner/taskservice/controllers/TaskController.java
+++ task-service/src/main/java/com/studyplanner/taskservice/controllers/TaskController.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @EnableAsync
