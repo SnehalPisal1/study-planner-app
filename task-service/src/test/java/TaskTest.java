dc7cffdf86d6a5f4fb77fc732fa6ccb31b6ddacb
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-05-23 17:26:16 +0200

    test

--- task-service/src/test/java/TaskTest.java
+++ task-service/src/test/java/TaskTest.java
@@ -3,6 +3,12 @@ import com.studyplanner.taskservice.models.Task;
 import com.studyplanner.taskservice.services.TaskServices;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import java.time.LocalDateTime;
 
 import static com.studyplanner.taskservice.models.TaskStatus.IN_PROGRESS;
