b4bc954ae627aa974369cb5021d4f598ac3e5958
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-05-26 15:23:56 +0200

    test

--- task-service/src/test/java/TaskTest.java
+++ task-service/src/test/java/TaskTest.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 
 import static com.studyplanner.taskservice.models.TaskStatus.IN_PROGRESS;
 
+@ExtendWith(MockitoExtension.class)
 public class TaskTest {
 
     @Mock
