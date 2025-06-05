25a91128392d4865ca47dfd20375941a17d8a080
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-06-05 14:05:09 +0200

    test

--- task-service/src/test/java/TaskTest.java
+++ task-service/src/test/java/TaskTest.java
@@ -13,6 +13,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.studyplanner.taskservice.models.TaskStatus.IN_PROGRESS;
 
