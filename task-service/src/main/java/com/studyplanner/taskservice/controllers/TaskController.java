6d2427d5585a2308107c1304bf6e1898cb92e87d
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-06-08 10:17:31 +0200

    refactor delete task API

--- task-service/src/main/java/com/studyplanner/taskservice/controllers/TaskController.java
+++ task-service/src/main/java/com/studyplanner/taskservice/controllers/TaskController.java
@@ -7,8 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import io.swagger.v3.oas.annotations.tags.Tag;
