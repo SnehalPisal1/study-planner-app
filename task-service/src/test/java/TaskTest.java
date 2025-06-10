6f1e11af59cb780d4e5cab0c6aa428bcc87d14e1
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-06-10 22:22:35 +0200

    refactor delete task API

--- task-service/src/test/java/TaskTest.java
+++ task-service/src/test/java/TaskTest.java
@@ -4,6 +4,7 @@ import com.studyplanner.taskservice.services.TaskServicesImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.studyplanner.taskservice.models.TaskStatus.DONE;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
